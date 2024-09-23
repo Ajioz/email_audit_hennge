4d02240eb1a24f1bb009324a9edb2820ba656e1a
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-23 22:59:22 +0100

    improved code base

--- src/components/RecipientsDisplay.tsx
+++ src/components/RecipientsDisplay.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useRef, useEffect } from 'react'
 import styled from 'styled-components'
-import RecipientsBadge from './RecipientsBadge' // import of RecipientsBadge
+import RecipientsBadge from './RecipientsBadge' // Assuming the badge is coming from here
 
+// Props for RecipientsDisplay
 type RecipientsDisplayProps = {
   recipients: string[]
 }
 
+// Wrapper for Recipients Display
 const RecipientsWrapper = styled.div`
   display: flex;
   align-items: center;
@@ -13,7 +15,41 @@ const RecipientsWrapper = styled.div`
   white-space: nowrap;
   text-overflow: ellipsis;
 `
+// Tooltip Components
+const TooltipWrapper = styled.div`
+  position: relative;
+  display: inline-block;
 
+  &:hover .tooltip-content {
+    display: block;
+  }
+`
+
+const TooltipContent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #333;
+  color: white;
+  text-align: left;
+  padding: 8px;
+  border-radius: 4px;
+  z-index: 1;
+`
+
+// Tooltip component to display all recipients
+export const RecipientTooltip: React.FC<{ recipients: string[] }> = ({
+  recipients,
+}) => {
+  return (
+    <TooltipWrapper>
+      <TooltipContent className="tooltip-content">
+        {recipients.join(', ')}
+      </TooltipContent>
+    </TooltipWrapper>
+  )
+}
+
+// Main Recipients Display component
 const RecipientsDisplay: React.FC<RecipientsDisplayProps> = ({
   recipients,
 }) => {
@@ -44,6 +80,7 @@ const RecipientsDisplay: React.FC<RecipientsDisplayProps> = ({
     setNumTruncated(hiddenCount)
   }, [recipients])
 
+  // Function to measure the width of text
   const measureTextWidth = (text: string, element: HTMLDivElement) => {
     const canvas = document.createElement('canvas')
     const context = canvas.getContext('2d')
@@ -62,6 +99,8 @@ const RecipientsDisplay: React.FC<RecipientsDisplayProps> = ({
         <>
           , ...
           <RecipientsBadge numTruncated={numTruncated} />
+          {/* Tooltip to show all recipients */}
+          <RecipientTooltip recipients={recipients} />
         </>
       )}
     </RecipientsWrapper>
