f7605347dd5f0ac979b2068a58d68a289d5186f3
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-09-24 14:40:13 +0100

    improving solution

--- src/components/RecipientsDisplay.tsx
+++ src/components/RecipientsDisplay.tsx
@@ -160,31 +160,3 @@ const RecipientsDisplay: React.FC<RecipientsDisplayProps> = ({
 export default RecipientsDisplay
 
 
-//////// track screen size /////
-
-export const ScreenSizeTracker: React.FC = () => {
-  const [width, setWidth] = useState<number>(window.innerWidth)
-  const [height, setHeight] = useState<number>(window.innerHeight)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth)
-      setHeight(window.innerHeight)
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
-  return (
-    <div>
-      <p>
-        Current screen size: {width}px x {height}px
-      </p>
-    </div>
-  )
-}
